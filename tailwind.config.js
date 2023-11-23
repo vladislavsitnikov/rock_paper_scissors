e667ab1ee10bafb44fa212b6b986c0608ce335d3
vladislavsitnikov <71400764+vladislavsitnikov@users.noreply.github.com>
2023-11-23 17:47:55 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue, js, ts, tsx, jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
